b8db297cdcd60c96bf979162a7916f0266a4ac88
davissandefur <davis.sandefur@gmail.com>
2015-04-21 16:33:16 -0500

    Fifth problem: Quantum Potential Step

---
+++ Chapter2/problem_five.py
@@ -0,0 +1,21 @@
+# Davis Sandefur
+# Completed on 21 April, 2015
+
+from math import sqrt, pow
+
+
+def probability(m, E, V):
+    h_bar = 1.05  # mmkg/s
+
+    k1 = sqrt(2*m*E)/h_bar
+    k2 = sqrt(2*m*(E-V))/h_bar
+
+    T = (4*k1*k2)/(k1+k2)**2  # Probability of transmission
+    R = ((k1-k2)/(k1+k2))**2  # Probability of reflection
+
+    return T, R
+
+if __name__ == '__main__':
+
+    Transmission, reflection = probability(9.11*pow(10, 11), 10, 9)
+    print(Transmission, reflectiongit )
